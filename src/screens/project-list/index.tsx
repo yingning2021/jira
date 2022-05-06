fe175a8ed0c89b0ab046c88bbf1e5fdf2ea22885
yingning2021 <yingning2021@163.com>
2022-05-06 17:34:06 +0800

    feat: 添加登陆状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,6 +3,7 @@ import { List } from "./list";
 import { useEffect, useState } from "react";
 import qs from "qs";
 import { cleanObject, useDebounce, useMount } from "../utils";
+import { useHttp } from "../utils/http";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
@@ -14,23 +15,14 @@ export const ProjectListScreen = () => {
 
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
+  const client = useHttp();
 
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
   return (
     <div>
